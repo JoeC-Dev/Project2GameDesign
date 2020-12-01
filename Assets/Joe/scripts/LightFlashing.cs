10e54175ea162816bcfcf72abb004e72b92419be
JoeyC <55225247+JoeCutrone@users.noreply.github.com>
2020-12-01 15:08:46 -0500

    working light

---
+++ Assets/Joe/scripts/LightFlashing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlashing : MonoBehaviour
+{
+    private int counter = 0;
+    [Header("Set in Inspector")]
+    public GameObject light;
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter % 100 == 0 && !light.activeSelf)
+            light.SetActive(true);
+        else if (counter % 100 == 0 && light.activeSelf)
+            light.SetActive(false);
+        
+
+    }
+}
