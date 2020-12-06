173d8ef8756281150e60d04b589dd33ea3063ec4
JoeyC <55225247+JoeCutrone@users.noreply.github.com>
2020-12-05 20:17:10 -0500

    .

---
+++ Assets/Joe/scripts/Launcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Vector3 originalPos;
+    public GameObject block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = block.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
