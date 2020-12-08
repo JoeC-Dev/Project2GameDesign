0a908bdae0574f6e6230bd0817c3d5fec1ad32b4
SevenPebbles <54554298+SevenPebbles@users.noreply.github.com>
2020-12-07 22:35:01 -0500

    Added paddle script

---
+++ Assets/Hoseman/Scripts/paddles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddles : MonoBehaviour
+{
+    public GameObject leftPaddle;
+    public GameObject rightPaddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            leftPaddle.transform.Rotate(-35.0f, 0.0f, 0.0f, Space.Self);
+            rightPaddle.transform.Rotate(35.0f, 0.0f, 0.0f, Space.Self);
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            leftPaddle.transform.Rotate(35.0f, 0.0f, 0.0f, Space.Self);
+            rightPaddle.transform.Rotate(-35.0f, 0.0f, 0.0f, Space.Self);
+        }
+    }
+}
