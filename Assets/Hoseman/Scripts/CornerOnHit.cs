1c48abec3f9ab7d579c3f50cf0ff1c2dbdc4db87
SevenPebbles <54554298+SevenPebbles@users.noreply.github.com>
2020-12-09 05:04:57 -0500

    Actualizing Corners

---
+++ Assets/Hoseman/Scripts/CornerOnHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerOnHit : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pebble")
+        {
+            other.GetComponent<Rigidbody>().velocity = new Vector3(x,y,z);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
